a0352c5a08af09afe5c8072180232671ef1d3044
Gaurav Shambharkar <shambharkargaurav04@gmail.com>
2026-08-08 15:00:51 +0530

    feat: integrate Vercel Analytics into the main application entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import ErrorBoundary from './Components/ErrorBoundary/ErrorBoundary.tsx'
-
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import ErrorBoundary from "./Components/ErrorBoundary/ErrorBoundary.tsx";
+import { Analytics } from "@vercel/analytics/react";
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
   </StrictMode>,
-)
+);
